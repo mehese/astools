3fd8f0b9cbf0489540b044132987134ebc7a044d
mehese <mehese@tcd.ie>
2013-03-30 19:18:21 +0000

    Added main function

---
+++ central.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def main() :
+    print 'Hello World!'
+
+
+# make sure the codelines don't get executed if we insert this as a module
+if __name__ == "__main__" :
+    main()
