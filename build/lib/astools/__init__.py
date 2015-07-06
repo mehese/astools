efac369f5ba5be67951597c6d81ef1d361492f67
Eric M <mehese@tcd.ie>
2015-07-06 16:19:30 +0100

    printing .xsf files to PrintStruct :repeat: + deleted build/ directory

--- build/lib/astools/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ['analysis', 'operations', 'ReadWrite', 'structures']
-
-from astools.analysis import *
-from astools.operations import *
-from astools.ReadWrite import *
-from astools.structures import *
